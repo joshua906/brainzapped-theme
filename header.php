0661f91762407096354a2c2e87c4ae2a5d8d489b
joshua906 <joshuamatthews906@yahoo.com>
2016-12-08 20:44:14 -0600

    added php to the beginning

--- header.php
+++ header.php
@@ -1,3 +1,15 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Fabthemes
+ */
+?>
+
 <!Doctype html>
 <head>
 <meta charset="utf-8">
