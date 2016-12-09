9c4a58b23e4716c806efece28aa4bdbde93fdd18
joshua906 <joshuamatthews906@yahoo.com>
2016-12-08 20:05:40 -0600

    named the theme file

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php get_header(); ?>
+<?php 
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one
+ * of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query,
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package bz
+ * @subpackage Sbz theme
+ * @since 
+ */
+
+ get_header(); ?>
 
 
 
